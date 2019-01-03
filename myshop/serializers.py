87c9a3cb94a9d938893ee8df03b3b825b3e76073
Jacob Rief <jacob.rief@gmail.com>
2019-01-03 10:28:15 +0100

    check if product is in cart

--- myshop/serializers.py
+++ myshop/serializers.py
@@ -4,9 +4,10 @@ from __future__ import unicode_literals
 from django.utils.safestring import mark_safe
 from rest_framework import serializers
 from rest_framework.fields import empty
+from shop.models.cart import CartModel
 from shop.search.serializers import ProductSearchSerializer as BaseProductSearchSerializer
 from shop.serializers.bases import ProductSerializer
-from shop.serializers.defaults import AddToCartSerializer
+from shop.serializers.defaults.catalog import AddToCartSerializer
 
 from myshop.models import SmartCard, SmartPhoneModel
 from myshop.search_indexes import myshop_search_index_classes
@@ -68,6 +69,10 @@ class AddSmartPhoneToCartSerializer(AddToCartSerializer):
     """
     def get_instance(self, context, data, extra_args):
         product = context['product']
+        try:
+            cart = CartModel.objects.get_from_request(context['request'])
+        except CartModel.DoesNotExist:
+            cart = None
         if data is empty:
             product_code = None
             extra = {}
@@ -83,6 +88,7 @@ class AddSmartPhoneToCartSerializer(AddToCartSerializer):
             'product': product.id,
             'product_code': variant.product_code,
             'unit_price': variant.unit_price,
+            'is_in_cart': bool(product.is_in_cart(cart, product_code=variant.product_code)),
             'extra': extra,
         }
         return instance
